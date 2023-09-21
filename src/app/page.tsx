3a2d81dc9b53af7396b1409f3ef5db39d23b5794
Brandon Garcia <grandonleebarcia@gmail.com>
2023-09-20 20:11:32 -0700

    added axios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import Image from 'next/image';
+import axios from 'axios';
 
 export default function Home() {
+  async function fetchData() {
+    const url =
+      'https://api.themoviedb.org/3/movie/550?api_key=503fc4d0a2e0cb386a656081a11f9b66';
+
+    const data = await axios(url);
+
+    console.log(data);
+  }
+
+  fetchData();
+
   return <main>Hello Worlds</main>;
 }
