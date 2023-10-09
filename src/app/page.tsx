2a6c37e559ed4ee0b67d9fe87f66adc510a03891
Brandon Garcia <grandonleebarcia@gmail.com>
2023-10-09 15:20:51 -0700

    started on landing page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@
 
 import Banner from './assests/Banner.jpg';
 import { useState } from 'react';
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import {
+  BsChevronCompactLeft,
+  BsChevronCompactRight,
+  BsPlayCircle,
+} from 'react-icons/bs';
 
 export default function Home() {
   const slides = [
@@ -43,19 +47,39 @@ export default function Home() {
   return (
     <main>
       <section className="h-screen ">
-        <div className="w-full h-[780px] m-auto  relative group">
+        <div className="w-full h-[780px] m-auto relative group">
           <div
             style={{ backgroundImage: `url(${Banner.src})` }}
             className="w-full h-full  bg-center bg-cover duration-500"
           >
-            <div className="w-full h-full absolute bg-gradient-radial from-transparent to-gray-500 opacity-60"></div>
+            <div className="w-full h-full bg-gradient-radial from-transparent to-gray-500 opacity-60 flex justify-center items-center">
+              <div className="w-3/6 h-3/6">
+                <div>
+                  <h1 className="text-6xl uppercase tracking-widest font-semibold">
+                    The Martian
+                  </h1>
+                </div>
+                <div className="w-1/5 flex justify-between uppercase">
+                  <div>adventure</div>
+                  <div>sci-fi</div>
+                  <div>thriller</div>
+                </div>
+                <div>
+                  <button className="w-1/6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full flex items-center justify-between">
+                    Watch Trailer <BsPlayCircle />
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
+
           <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
             <BsChevronCompactLeft onClick={prevSlide} />
           </div>
           <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
             <BsChevronCompactRight onClick={nextSlide} />
           </div>
+          <div></div>
         </div>
       </section>
     </main>
