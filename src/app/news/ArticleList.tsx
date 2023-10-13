0cfb1300e1f6371e2852935da8c2a3fee7a00e42
Brandon Garcia <grandonleebarcia@gmail.com>
2023-10-13 14:30:28 -0700

    added new page route for news

---
+++ src/app/news/ArticleList.tsx
@@ -0,0 +1,11 @@
+const getArticles = async () => {
+  // const response = await fetch(
+  //   `https://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=${NEXT_PUBLIC_}`
+  // );
+};
+
+const ArticleList = () => {
+  return <div>ArticleList</div>;
+};
+
+export default ArticleList;
---
+++ src/app/news/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <h1>News Articles</h1>
+    </div>
+  );
+};
+
+export default page;
